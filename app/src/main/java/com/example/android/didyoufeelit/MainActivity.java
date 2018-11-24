454c27bd0f1b25da74cb732c0725545d5fab0e1c
Jerry Chong <15714095+jerrychong25@users.noreply.github.com>
2018-11-24 21:38:35 +0800

    Fixed crash issue by running network in background thread

--- app/src/main/java/com/example/android/didyoufeelit/MainActivity.java
+++ app/src/main/java/com/example/android/didyoufeelit/MainActivity.java
@@ -15,6 +15,7 @@
  */
 package com.example.android.didyoufeelit;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
@@ -34,11 +35,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Perform the HTTP request for earthquake data and process the response.
-        Event earthquake = Utils.fetchEarthquakeData(USGS_REQUEST_URL);
-
-        // Update the information displayed to the user.
-        updateUi(earthquake);
+        // Fetch earthquake data in background thread
+        EarthquakeAsyncTask task = new EarthquakeAsyncTask();
+        task.execute(USGS_REQUEST_URL);
     }
 
     /**
@@ -53,5 +52,23 @@ public class MainActivity extends AppCompatActivity {
 
         TextView magnitudeTextView = (TextView) findViewById(R.id.perceived_magnitude);
         magnitudeTextView.setText(earthquake.perceivedStrength);
+    }
+
+    private class EarthquakeAsyncTask extends AsyncTask<String, Void, Event> {
+
+        @Override
+        protected Event doInBackground(String... urls) {
+
+            // Perform the HTTP request for earthquake data and process the response.
+            Event result = Utils.fetchEarthquakeData(urls[0]);
+
+            return result;
+        }
+
+        @Override
+        protected void onPostExecute(Event result) {
+            // Update the information displayed to the user.
+            updateUi(result);
+        }
     }
 }
